52741f362d3a3eac38cf2bbff1ce522b65741878
jem <91963927+jemcv@users.noreply.github.com>
2023-03-24 22:56:08 +0800

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MainApp());
 }
+
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
@@ -17,4 +25,7 @@ class MainApp extends StatelessWidget {
       ),
     );
   }
+    
 }
+
+
