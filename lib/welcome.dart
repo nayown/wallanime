acc2bd2b5edb91ffb378a56ea7c6160e08e6858f
jem <91963927+jemcv@users.noreply.github.com>
2023-03-26 22:02:19 +0800

    update welcome

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -12,21 +12,22 @@ class Welcome extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-             const Text(
+            const Text(
               'AnimeScroller',
               style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold),
+                  color: Colors.red, fontSize: 24, fontWeight: FontWeight.bold),
             ),
-             const SizedBox(
+            const SizedBox(
               height: 10,
             ),
-             const Image(image: AssetImage('assets/images/hellsing.jpg'),  width: 400,), // height 600
-             const SizedBox(
+            const Image(
+              image: AssetImage('assets/images/hellsing.jpg'),
+              width: 400,
+            ), // height 600
+            const SizedBox(
               height: 40,
             ),
-             const Padding(
+            const Padding(
               padding: EdgeInsets.only(),
               child: Text(
                 'Never run out of new anime to watch because every time you scroll through the app, a brand new anime suggestion will appear.',
@@ -40,9 +41,21 @@ class Welcome extends StatelessWidget {
             const SizedBox(
               height: 60,
             ),
-            ElevatedButton(onPressed: () { { 
-              Navigator.push(context,  MaterialPageRoute(builder: (context) => Login()),);
-            }}, child: Text("Get Started"), style: ElevatedButton.styleFrom(backgroundColor: Colors.red, padding: EdgeInsets.only(left: 100, right: 100, top: 20, bottom: 20))),
+            ElevatedButton(
+               child: Text("Get Started"),
+                onPressed: () {
+                  {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Login()),
+                    );
+                  }
+                },
+               
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    padding: const EdgeInsets.only(
+                        left: 100, right: 100, top: 20, bottom: 20))),
           ],
         ),
       ),
