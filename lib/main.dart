76e030d38595f07b2eeafd29d9b69129de0d431e
jem <91963927+jemcv@users.noreply.github.com>
2023-03-28 01:42:26 +0800

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:hackanime/welcome.dart';
+import 'package:hackanime/widgets/welcome.dart';
 import 'firebase_options.dart';
 
 void main() async {
