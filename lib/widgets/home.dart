cc96f463a28420227ee22c2f8f32d490230d5eb6
jem <91963927+jemcv@users.noreply.github.com>
2023-03-28 01:42:30 +0800

    create home

---
+++ lib/widgets/home.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(title: const Text("AnimeScroller", style: TextStyle(color: Colors.red),), automaticallyImplyLeading: false, backgroundColor: Colors.black,),
+      //IInsert Main Content Scroll Feature
+      bottomNavigationBar: BottomNavigationBar(
+        //showSelectedLabels: false,
+        //showUnselectedLabels: false,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Colors.red,
+        backgroundColor: Colors.black,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_rounded),
+            label: 'Profile',
+          ),
+        ],
+),
+    );
+  }
+}
