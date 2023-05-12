a051794e71d2f7440a0e3021c247900295db01b4
jem <91963927+jemcv@users.noreply.github.com>
2023-05-13 05:08:49 +0800

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:hackanime/screens/welcome.dart';
 import 'firebase_options.dart';
 
-// For Initializing Firebase
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -11,11 +10,10 @@ void main() async {
   );
 
   runApp(MaterialApp(
-      title: "HackAnime",
-      theme: ThemeData.dark(),
-      darkTheme: ThemeData.dark(),
-      debugShowCheckedModeBanner: false,
-      home: const Welcome(),
-      ),
-    );
+    title: "HackAnime",
+    theme: ThemeData.dark(),
+    darkTheme: ThemeData.dark(),
+    home: const Welcome(),
+    debugShowCheckedModeBanner: false, // Add this line to remove the debug banner
+  ));
 }
