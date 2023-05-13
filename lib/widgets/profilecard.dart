8ab7d2c424c1a0089e0229189591a1bf026c1b12
jem <91963927+jemcv@users.noreply.github.com>
2023-05-14 01:31:08 +0800

    update profile

--- lib/widgets/profilecard.dart
+++ lib/widgets/profilecard.dart
@@ -10,23 +10,66 @@ class ProfileCard extends StatefulWidget {
 
 class _ProfileCardState extends State<ProfileCard> {
   final currentUser = FirebaseAuth.instance.currentUser;
+  final TextEditingController emailController = TextEditingController();
 
   void _logout() async {
     await FirebaseAuth.instance.signOut();
   }
 
   @override
+  void initState() {
+    super.initState();
+    emailController.text = currentUser?.email ?? '';
+  }
+
+@override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Email: ${currentUser?.email ?? ''}'),
-            SizedBox(height: 16.0),
-          ],
-        ),
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              margin: EdgeInsets.only(top: 50.0),
+              child: CircleAvatar(
+                radius: 100.0,
+                backgroundImage: NetworkImage(
+                    'https://w.wallhaven.cc/full/8x/wallhaven-8xlpg2.jpg'),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 260.0),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextField(
+                    controller: emailController,
+                    style: const TextStyle(color: Colors.black),
+                    decoration: InputDecoration(
+                      enabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
+                      ),
+                      focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintText: 'Email Address',
+                      prefixIcon: const Icon(Icons.email),
+                      prefixIconColor: Colors.red,
+                      hintStyle: TextStyle(color: Colors.grey[500]),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16.0),
+              ],
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _logout,
